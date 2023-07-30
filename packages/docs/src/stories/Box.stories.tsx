1d877e662d728f4a41b65957cb5e0ce3f5ed2a7c
Diego-Lopes <diegolopes2009@hotmail.com>
2023-07-29 22:06:05 -0400

    ♻️refactore: modification component of children strong for component Text

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -1,16 +1,12 @@
 import type { StoryObj, Meta } from '@storybook/react'
 
-import { Box, BoxProps } from '@ignite-ui-diego/react'
+import { Box, BoxProps, Text } from '@ignite-ui-diego/react'
 
 export default {
   title: 'Surfaces/Box',
   component: Box,
   args: {
-    children: (
-      <>
-        <span>Testando o elemento box</span>
-      </>
-    ),
+    children: <Text>Testando o elemento box</Text>,
   },
 } as Meta<BoxProps>
 
