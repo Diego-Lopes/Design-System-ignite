cbd7d14643a7159b892b5b0625751b576576f759
Diego-Lopes <diegolopes2009@hotmail.com>
2023-07-29 22:06:35 -0400

    ✨feat: created component and stories Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,21 @@
+import type { StoryObj, Meta } from '@storybook/react'
+
+import { Text, TextProps } from '@ignite-ui-diego/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut fuga explicabo consequatur autem pariatur reprehenderit, facilis illum exercitationem, quam possimus enim delectus et esse odio nisi vel placeat, facere quis!',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Custom strong',
+    as: 'strong',
+  },
+}
