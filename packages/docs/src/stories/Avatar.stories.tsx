7a37b117dfd5d5dbd09214208fc1d7a310c68abc
diego-lopes <diegolopes2009@hotmail.com>
2023-08-03 17:38:21 -0400

    ✨feat: created component Avatar

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+
+import { Avatar, AvatarProps } from '@ignite-ui-diego/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  tags: ['autodocs'],
+  args: {
+    src: 'https://github.com/diego-lopes.png',
+    alt: 'Diego Lopes',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
