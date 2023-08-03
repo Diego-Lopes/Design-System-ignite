631d68cd14fd4c9f7cbcdc4c5cec4ce89a0cacc6
diego-lopes <diegolopes2009@hotmail.com>
2023-08-03 18:08:05 -0400

    ✨feat: created component Button

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,25 @@
+import type { StoryObj, Meta } from '@storybook/react'
+
+import { Button, ButtonProps } from '@ignite-ui-diego/react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  tags: ['autodocs'],
+  args: {
+    children: 'Send',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'secondary',
+  },
+}
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'tertiary',
+    children: 'Cancel',
+  },
+}
