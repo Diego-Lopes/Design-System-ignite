5acc35fb031773d170c0173a32f1cb90ce73716f
diego-lopes <diegolopes2009@hotmail.com>
2023-08-04 08:09:32 -0400

    ♻️refactore: finished custom TextInput

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -1,15 +1,42 @@
 import type { StoryObj, Meta } from '@storybook/react'
 
-import { TextInput, TextInputProps } from '@ignite-ui-diego/react'
+import { Box, Text, TextInput, TextInputProps } from '@ignite-ui-diego/react'
 
 export default {
   title: 'Form/Text Input',
   component: TextInput,
   tags: ['autodocs'],
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '$2',
+          }}
+        >
+          <Text size={'sm'}>Username</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
   args: {},
 } as Meta<TextInputProps>
 
-export const Primary: StoryObj<TextInputProps> = {}
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'Type your name',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
 
 export const WithPrefix: StoryObj<TextInputProps> = {
   args: {
