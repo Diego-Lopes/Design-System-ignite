006816308e6dcf03994c04744161d8245fdb797a
diego-lopes <diegolopes2009@hotmail.com>
2023-08-04 09:54:18 -0400

    ✨feat: added component Checkbox

---
+++ packages/docs/src/stories/Checbox.stories.tsx
@@ -0,0 +1,30 @@
+import type { StoryObj, Meta } from '@storybook/react'
+
+import { Box, Text, Checkbox, CheckboxProps } from '@ignite-ui-diego/react'
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            gap: '$2',
+          }}
+        >
+          {Story()}
+          <Text size={'sm'}>Accept terms of use</Text>
+        </Box>
+      )
+    },
+  ],
+  args: {},
+} as Meta<CheckboxProps>
+
+export const Primary: StoryObj<CheckboxProps> = {}
