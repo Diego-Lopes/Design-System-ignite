c25e9cdbdd2cb37f9ad59d27cfa23441254fe89b
diego-lopes <diegolopes2009@hotmail.com>
2023-08-04 09:11:14 -0400

    ✨feat: created component text area

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,39 @@
+import type { StoryObj, Meta } from '@storybook/react'
+
+import { Box, Text, TextArea, TextAreaProps } from '@ignite-ui-diego/react'
+
+export default {
+  title: 'Form/Text Area',
+  component: TextArea,
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '$2',
+          }}
+        >
+          <Text size={'sm'}>Observation</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+  args: {},
+} as Meta<TextAreaProps>
+
+export const Primary: StoryObj<TextAreaProps> = {
+  args: {
+    placeholder: 'Add any observations...',
+  },
+}
+
+export const Disabled: StoryObj<TextAreaProps> = {
+  args: {
+    disabled: true,
+  },
+}
